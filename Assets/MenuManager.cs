d8ff7d78e552a9317e2fcf137d8ee33e36b5cf65
Christoph Graf <c.graf@sae.edu>
2019-07-17 12:22:46 +0200

    fu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class MenuManager : MonoBehaviour
+{
+    public int DefaultLoadLevel = 2;
+    public static int LoadLevel { get; private set; }
+    
+    // Update is called once per frame
+    void Start()
+    {
+        LoadLevel = DefaultLoadLevel;
+        UnityEngine.RemoteSettings.Completed += HandleRemoteSettings;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(LoadLevel);
+    }
+
+    private void HandleRemoteSettings(bool wasUpdatedFromServer, bool settingsChanged, int serverResponse) {
+        LoadLevel = UnityEngine.RemoteSettings.GetInt("start_level", DefaultLoadLevel);
+
+    }
+}
